b28d2fce6805f0bace788f2121e8cb16b4eae6c4
Adego Omwoa <adegomwoa@gmail.com>
2024-08-16 14:28:49 +0300

    added pwds

--- app.js
+++ app.js
@@ -192,4 +192,17 @@ document.addEventListener("DOMContentLoaded", () => {
         tooltip.style.display === "block" ? "none" : "block";
     });
   }
+
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js')
+        .then(registration => {
+          console.log('Service Worker registered with scope:', registration.scope);
+        })
+        .catch(err => {
+          console.error('Service Worker registration failed:', err);
+        });
+    });
+  }
+  
 });
